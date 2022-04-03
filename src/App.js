51cffd47edf4a06c8332cf15663021ed1ddacfd6
frsiam <siam.frs@gmail.com>
2022-04-03 15:37:36 +0600

    initialize the blog template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import { createContext } from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from './Components/Home/Home';
+import Videos from './Components/Videos/Videos';
+import Login from './Components/Login/Login'
+import NotFound from './Components/NotFound/NotFound';
 
-export const BlogContext = createContext();
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+    <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/videos' element={<Videos></Videos>} />
+        <Route path='/login' element={<Login />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
